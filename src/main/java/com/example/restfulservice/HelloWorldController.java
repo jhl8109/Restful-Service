3a11b4d6b70ef094cfa8dd3024684e8ed73ac21d
jhl8109 <jhl81094@gmail.com>
2022-11-15 20:16:25 +0900

    update path variable

---
+++ src/main/java/com/example/restfulservice/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.example.restfulservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // @RequestMapping(method=RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String HelloWorld() {
+        return "Hello World";
+    }
+
+    //bean 형태를 사용함으로써 단순 text, string 형태가 아닌 json 형태로 반환해주게 된다.
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean HelloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean HelloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World %s",name));
+    }
+}
