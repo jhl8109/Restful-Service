1a26d621df83406e8ca7cbcf49a3ec676239e7db
jhl8109 <jhl81094@gmail.com>
2022-11-16 23:58:14 +0900

    add parameterException, internationalization using locale

--- src/main/java/com/example/restfulservice/RestfulServiceApplication.java
+++ src/main/java/com/example/restfulservice/RestfulServiceApplication.java
@@ -2,6 +2,12 @@ package com.example.restfulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.annotation.ManagedBean;
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulServiceApplication {
@@ -10,4 +16,10 @@ public class RestfulServiceApplication {
         SpringApplication.run(RestfulServiceApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
